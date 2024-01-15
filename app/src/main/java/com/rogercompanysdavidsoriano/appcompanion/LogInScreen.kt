d0ae2aeeb7c9dcc83ae44fc88ef29cef7a232416
RoheSama <roger.companys@enti.cat>
2024-01-15 23:46:18 +0100

    Firebase analytics works perfect

---
+++ app/src/main/java/com/rogercompanysdavidsoriano/appcompanion/LogInScreen.kt
@@ -0,0 +1,12 @@
+package com.rogercompanysdavidsoriano.appcompanion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginscreen)
+
+    }
+}
---
+++ app/src/main/java/com/rogercompanysdavidsoriano/appcompanion/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.rogercompanysdavidsoriano.appcompanion
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.analytics.FirebaseAnalytics
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Event per provar firebase Analytics
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integració Firebase correcta")
+        analytics.logEvent("InitScreen", bundle)
+
+        //Boto que t'envia a login screen
+        val button = findViewById<Button>(R.id.main_button)
+
+        button.setOnClickListener{
+            val intent = Intent(this, LogInScreen::class.java)
+            startActivity(intent)
+        }
+    }
+}
