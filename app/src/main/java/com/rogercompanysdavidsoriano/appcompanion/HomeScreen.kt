5bc243f37f2563971922fccebd049c3d8dec0a4c
RoheSama <roger.companys@enti.cat>
2024-01-16 04:29:00 +0100

    Login with google implemented

--- app/src/main/java/com/rogercompanysdavidsoriano/appcompanion/HomeScreen.kt
+++ app/src/main/java/com/rogercompanysdavidsoriano/appcompanion/HomeScreen.kt
@@ -1,10 +1,16 @@
 package com.rogercompanysdavidsoriano.appcompanion
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 import java.nio.channels.spi.AsynchronousChannelProvider
+
+enum class ProviderType {
+    BASIC,
+    GOOGLE
+}
 
 class HomeScreen : AppCompatActivity() {
 
@@ -15,12 +21,29 @@ class HomeScreen : AppCompatActivity() {
 
         logOutButton = findViewById(R.id.logOutButton)
 
-        setup()
+        // Dades del usuari autenticat
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+
+        setup(email ?: "", provider ?: "")
+
+        // Guardar dades del usuari al fitxer strings
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
     }
 
-    private fun setup(){
+    private fun setup(email: String, provdirer: String){
 
         logOutButton.setOnClickListener{
+
+            // Quan tanquem sessio, borrem les dades guardades del usuari
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
--- app/src/main/java/com/rogercompanysdavidsoriano/appcompanion/LogInScreen.kt
+++ app/src/main/java/com/rogercompanysdavidsoriano/appcompanion/LogInScreen.kt
@@ -1,30 +1,61 @@
 package com.rogercompanysdavidsoriano.appcompanion
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.FilterQueryProvider
+import android.widget.LinearLayout
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LogInScreen : AppCompatActivity() {
 
+    private val GOOGLE_SIGN_IN = 100
+
     private lateinit var signUpButton: Button
     private lateinit var logInButton: Button
+    private lateinit var googleButton: Button
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
+    private lateinit var authLayout: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginscreen)
 
         signUpButton = findViewById(R.id.singUpButton)
+        googleButton = findViewById(R.id.googleButton)
         logInButton = findViewById(R.id.logInButton)
         emailEditText = findViewById(R.id.emailEditText)
         passwordEditText = findViewById(R.id.passwordEditText)
+        authLayout = findViewById(R.id.authLayout)
 
         setup()
+        session()
+    }
+
+    override fun onStart(){
+        super.onStart()
+
+        authLayout.visibility = View.VISIBLE
+    }
+
+    private fun session(){
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+        // Comprovem si teniam dades de sessio emmagatzemades
+        if(email!= null && provider != null){
+            authLayout.visibility = View.INVISIBLE
+            showHome(email, ProviderType.valueOf(provider))
+        }
     }
 
     private fun setup(){
@@ -39,7 +70,7 @@ class LogInScreen : AppCompatActivity() {
                 ).addOnCompleteListener() {
                     if (it.isSuccessful) {
                         //Si el log in funciona, t'envia a l'activitat principal
-                        showHome()
+                        showHome(it.result?.user?.email ?: "",ProviderType.BASIC)
                     } else {
                         //Si el login falla, missatge d'alerta
                         showAlert()
@@ -56,13 +87,25 @@ class LogInScreen : AppCompatActivity() {
                     passwordEditText.text.toString()).addOnCompleteListener(){
                     if(it.isSuccessful){
                         //Si el log in funciona, t'envia a l'activitat principal
-                        showHome()
+                        showHome(it.result?.user?.email ?: "",ProviderType.BASIC)
                     }else{
                         //Si el login falla, missatge d'alerta
                         showAlert()
                     }
                 }
             }
+        }
+
+        googleButton.setOnClickListener {
+            // Configuracio de autenticacio
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.app_name))
+                .requestEmail()
+                .build()
+
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
         }
     }
 
@@ -76,9 +119,40 @@ class LogInScreen : AppCompatActivity() {
         dialog.show()
     }
 
-    private fun showHome(){
+    private fun showHome(email: String, providerType: ProviderType){
         // Carregar activitat principal de l'aplicació
-        val homeIntent = Intent(this, HomeScreen::class.java)
+        val homeIntent = Intent(this, HomeScreen::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", providerType)
+        }
         startActivity(homeIntent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == GOOGLE_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if(account != null) {
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(){
+                        if(it.isSuccessful){
+                            //Si el log in funciona, t'envia a l'activitat principal
+                            showHome( account.email ?: "",ProviderType.GOOGLE)
+                        }else{
+                            //Si el login falla, missatge d'alerta
+                            showAlert()
+                        }
+                    }
+                }
+            }catch (e:ApiException){
+                showAlert()
+            }
+
+        }
     }
 }
